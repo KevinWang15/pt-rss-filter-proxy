87c6e68988660cea8689e10ba8ba13f6016abb7a
Ke Wang <453587854@qq.com>
2017-08-20 19:52:39 +0800

    update config specs

--- config-example/hdsky.js
+++ config-example/hdsky.js
@@ -1,3 +1,10 @@
 module.exports = {
     cookie: "c_secure_uid=***; c_secure_pass=***; c_secure_ssl=***; c_secure_tracker_ssl=***; c_secure_login=***; UM_distinctid=***; CNZZDATA***=***",
+
+    // only show freeleech torrents
+    freeleech: true,
+
+    // in MB, only show torrents with size less than 5000MB (0 for unlimited)
+    fileSize: 5000,
+
 };
--- config-example/m-team.js
+++ config-example/m-team.js
@@ -1,3 +1,10 @@
 module.exports = {
     cookie: "tp=NT****",
+
+    // only show freeleech torrents
+    freeleech: true,
+
+    // in MB, only show torrents with size less than 5000MB (0 for unlimited)
+    fileSize: 5000,
+
 };
