8356b239e1b46dad196072dba6af720beb0ed487
Ke Wang <453587854@qq.com>
2017-08-20 19:58:32 +0800

    update config specs

--- config-example/chdbits.js
+++ config-example/chdbits.js
@@ -2,9 +2,9 @@ module.exports = {
     cookie: "__cfduid=***; c_secure_uid=***; c_secure_pass=***; c_secure_ssl=***; c_secure_tracker_ssl=***; c_secure_login=***",
 
     // only show freeleech torrents
-    freeleech: true,
+    onlyWhenFreeLeech: true,
 
     // in MB, only show torrents with size less than 5000MB (0 for unlimited)
-    fileSize: 5000,
+    onlyWhenFileSizeInMBLessThan: 5000,
 
 };
--- config-example/m-team.js
+++ config-example/m-team.js
@@ -2,9 +2,9 @@ module.exports = {
     cookie: "tp=NT****",
 
     // only show freeleech torrents
-    freeleech: true,
+    onlyWhenFreeLeech: true,
 
     // in MB, only show torrents with size less than 5000MB (0 for unlimited)
-    fileSize: 5000,
+    onlyWhenFileSizeInMBLessThan: 5000,
 
 };
