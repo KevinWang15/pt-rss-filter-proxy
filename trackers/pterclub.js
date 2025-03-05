666c49fa22513fa75fbcf3b9086ffef8da37628f
Ke Wang <me@ke.wang>
2025-03-05 08:57:34 +0800

    feat: add pterclub

---
+++ trackers/pterclub.js
@@ -0,0 +1,6 @@
+export default {
+    domain: "pterclub.com",
+    regex_check_page: "https?://pterclub\\.com/details\\.php\\?id=(\\d+)",
+    regex_size_field: "大小：</b></b>([\\d.]+\\s*[GMK]B)",
+    regex_check_page_freeleech_test:"免费&lt;/font",
+};
